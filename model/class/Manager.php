50de7901f710456239d14dab97aaf2b84c6671df
Komche <Adamoukomcheabdoulrazak@gmail.com>
2020-08-21 14:27:53 +0100

    arragenment de requête

--- model/class/Manager.php
+++ model/class/Manager.php
@@ -12,7 +12,7 @@ class Manager
 
     public static function database()
     {
-        $dbname = 'akoybizc_checke';
+        $dbname = 'akoybizc_checker';
         $user = 'akoybizc_komche';
         $pass = '@damoukomche2019';
         $host = 'localhost';
@@ -34,7 +34,7 @@ class Manager
 
     public static function bdd()
     {
-        $dbname = 'akoybizc_baseaemn';
+        $dbname = 'akoybizc_checker';
         $user = 'akoybizc_komche';
         $pass = '@damoukomche2019';
         $host = 'localhost';
