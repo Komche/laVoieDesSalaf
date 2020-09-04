51527c5eba4f048b475c0ad40418094b9302073d
Komche <Adamoukomcheabdoulrazak@gmail.com>
2020-09-04 13:06:04 +0100

    model designe code et doc api webservice

--- public/js/data_handler.js
+++ public/js/data_handler.js
@@ -185,7 +185,7 @@ function getModel(uniqueId) {
 
             });
             $('#body_model').html($dataModel);
-            $('#param').append($param);
+            $('#param').append(param);
         },
         error: function (xhr, resp, text) {
             //  error to console
