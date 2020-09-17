263900aa4f3ccb1b9b9a65dd5ad55970a19931c6
Komche <Adamoukomcheabdoulrazak@gmail.com>
2020-09-17 11:03:31 +0100

    amélioration doc qr

--- view/getDocumentView.php
+++ view/getDocumentView.php
@@ -68,7 +68,8 @@ $keys = array_keys($document);
                             <div class="invoice-billing">
                                 <div class="row">
                                     <div class="col-lg-12">
-                                        <div class="invoice-address align-center">
+                                        <div class="invoice-address d-flex justify-content-cente">
+                                            <h2 class="mb-3"> <i style="color:mediumseagreen" class="fa fa-check-circle"></i> VERIFIE</h2>
                                             <h4 class="mb-3"> <i style="color:mediumseagreen" class="fa fa-check-circle"></i> Document</h4>
                                             <ul class="list-unstyled">
                                                 <?php
