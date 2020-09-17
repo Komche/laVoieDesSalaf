6b1657227ce76e74a38a0f604fd58093bc753e63
Komche <Adamoukomcheabdoulrazak@gmail.com>
2020-09-17 07:29:05 +0100

    list des touts les documentd

--- view/searchView.php
+++ view/searchView.php
@@ -21,9 +21,64 @@ ob_start();
 </div>
 <div class="container container-margin">
   <div id="#searchResult" class="row">
-  
+    <div id="allDoc">
+      <?php
+      $data = file_get_contents(FIRESTORE_PATH . "/documents");
+      // var_dump(json_decode($data, true));
+      $data = json_decode($data, true)['documents'];
+      if (is_array($data) || is_object($data)) {
+        foreach ($data as $key => $value) {
+          // var_dump($value);
+          // echo  $data[$key];
+          $value = $value['fields'];
+          // if ($key == 'imgpath') {
+          unset($value['model']);
+          unset($value['matricule']);
+          unset($value['entity']);
+
+
+      ?>
+          <div class="col-md-4">
+            <div class="card m-b-30">
+              <div class="card-header with-border">
+                <h3 class="card-title"><?= $title ?></h3>
+              </div>
+              <!-- /.card-header -->
+              <div class="card-body">
+                <a href="https://checker.akoybiz.com/index.php?mat=<?= $value['matricule']['stringValue'] ?>" target="_blank" rel="noopener noreferrer">Voir plus</a>
+                <ul class="list-group">
+                  <?php
+                  unset($value['imgpath']);
+                  unset($value['documentQrpath']);
+                  $keys = array_keys($value);
+                  foreach ($keys as $k => $v) {
+
+
+                  ?>
+                    <li class="list-group-item"><?= ($v == 'entity_matricule') ? 'ref: ' . $value[$v]['stringValue'] : $v . ": " . $value[$v]['stringValue'] ?></li>
+                  <?php } ?>
+                </ul>
+              </div>
+              <!-- /.card-body -->
+              <div class="card-footer clearfix">
+                </ul>
+              </div>
+            </div>
+          </div>
+
+      <?php
+
+        }
+      }
+      ?>
+    </div>
   </div>
 </div>
+<script>
+  $('#button-addon2').on('click', function() {
+
+  });
+</script>
 <?php
 $content = ob_get_clean();
 require('template.php');
