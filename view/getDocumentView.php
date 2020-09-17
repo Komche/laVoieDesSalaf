a007758dad9e91bfec227661ae30832dfe1a0316
Komche <Adamoukomcheabdoulrazak@gmail.com>
2020-09-17 11:09:24 +0100

    amélioration doc qr

--- view/getDocumentView.php
+++ view/getDocumentView.php
@@ -68,17 +68,20 @@ $keys = array_keys($document);
                             <div class="invoice-billing">
                                 <div class="row">
                                     <div class="col-lg-12">
-                                        <div class="invoice-address d-flex justify-content-center">
-                                            <h2 class="mb-3"> <i style="color:mediumseagreen" class="fa fa-check-circle"></i> VERIFIE</h2>
-                                            <h4 class="mb-3"> <i style="color:mediumseagreen" class="fa fa-check-circle"></i> Document</h4>
-                                            <ul class="list-unstyled">
-                                                <?php
-                                                foreach ($keys as $k => $v) {
-                                                ?>
-                                                    <li> <b style="font-size: large;"><?= ($v == 'entity_matricule') ? 'ref: ' . $document[$v]['stringValue'] : str_replace("_"," ",$v) . ": " . $document[$v]['stringValue'] ?></b></li>
-                                                <?php } ?>
-                                               
-                                            </ul>
+                                        <div class="col-lg-offset-2 col-lg-4 col-lg-offset-2">
+
+                                            <div class="invoice-address">
+                                                <h2 class="mb-3"> <i style="color:mediumseagreen" class="fa fa-check-circle"></i> VERIFIE</h2>
+                                                <h4 class="mb-3">  Document</h4>
+                                                <ul class="list-unstyled">
+                                                    <?php
+                                                    foreach ($keys as $k => $v) {
+                                                    ?>
+                                                        <li> <b style="font-size: large;"><?= ($v == 'entity_matricule') ? 'ref: ' . $document[$v]['stringValue'] : str_replace("_"," ",$v) . ": " . $document[$v]['stringValue'] ?></b></li>
+                                                    <?php } ?>
+                                                   
+                                                </ul>
+                                            </div>
                                         </div>
                                     </div>
                                    
