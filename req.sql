fd88ddf46905edabb1b6d57a28baf7f1f5f4c029
227Mahaman <zalkiabass.456@gmail.com>
2020-10-10 17:29:15 +0100

    date annonce to timestamp

--- req.sql
+++ req.sql
@@ -1,1 +1,3 @@
 ALTER TABLE `annonces` ADD `date_event` DATE NULL AFTER `users`, ADD `time_event` TIME NULL AFTER `date_event`;
+
+ALTER TABLE `annonces` CHANGE `date_annonce` `date_annonce` TIMESTAMP NOT NULL;
